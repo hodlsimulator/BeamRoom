53ac799f62f3c3707d24206df95f873df81c1b34
Conor <hodlsimulator@gmail.com>
2025-09-21 00:44:21 +0100

    Initial Commit

---
+++ BeamRoomHost/BeamRoomHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeamRoomHostApp.swift
+//  BeamRoomHost
+//
+//  Created by . . on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BeamRoomHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
